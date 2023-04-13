517f55f94fdca2fa67df0c767f0a6ab8a17b4d32
Roberto Marungo <robmarungo.dev@gmail.com>
2023-04-13 15:05:43 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+        const input = document.querySelector("input#searchByID");
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        if (input.value < 4) {
+            fetch(`http://localhost:3000/movies/${input.value}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    title.innerText = data.title;
+                    summary.innerText = data.summary;
+                });
+        } else {
+            title.innerText = "Please Enter A Valid ID";
+            summary.innerText = "";
+        }
+    });
+};
+
+document.addEventListener("DOMContentLoaded", init);
